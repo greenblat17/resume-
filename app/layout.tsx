6c0a9c35f25d70b112aa2597b80de282c78eb4a7
Alex Zhuravlev <leftemail17@mail.ru>
2025-05-22 22:56:58 +0300

    Enhance RootLayout by adding favicon and suppressing hydration warning for improved performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,38 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Alexander Zhuravlev | Software Engineer & Tech-Founder",
-  description: "Portfolio of Alexander Zhuravlev, a tech founder and full-stack engineer",
-    generator: 'v0.dev'
-}
+  description:
+    "Portfolio of Alexander Zhuravlev, a tech founder and full-stack engineer",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/ava2.jpg" />
+      </head>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
